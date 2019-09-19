e40393c71787e5cf38b84f2115ca27277db5cca2
takagi <209098532@qq.com>
2019-09-19 17:13:07 +0800

    抽取公共页面片段和查询员工列表的实现

---
+++ boot-web/src/main/java/com/takagi/bootweb/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.takagi.bootweb.controller;
+
+import com.takagi.bootweb.dao.EmployeeDao;
+import com.takagi.bootweb.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @GetMapping("/emps")
+    public String list(Model model) {
+        Collection<Employee> emps = employeeDao.getAll();
+        model.addAttribute("emps", emps);
+        return "list";
+    }
+}
