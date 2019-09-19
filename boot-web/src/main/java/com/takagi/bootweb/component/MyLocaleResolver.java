c3a95e159c27b18dd654488692c868b7eb5ab11c
takagi <209098532@qq.com>
2019-09-19 11:36:55 +0800

    实现国际化信息

---
+++ boot-web/src/main/java/com/takagi/bootweb/component/MyLocaleResolver.java
@@ -0,0 +1,29 @@
+package com.takagi.bootweb.component;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+public class MyLocaleResolver implements LocaleResolver {
+    @Override
+    public Locale resolveLocale(HttpServletRequest httpServletRequest) {
+        //得到默认的区域解析器
+        Locale locale = Locale.getDefault();
+        //获取请求的语言参数
+        String l = httpServletRequest.getParameter("l");
+        //0为语言信息，1为区域信息
+        String[] s = l.split("_");
+        if (!StringUtils.isEmpty(l)) {
+            locale = new Locale(s[0],s[1]);
+        }
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Locale locale) {
+
+    }
+}
