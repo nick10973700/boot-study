f9b07ddac21b0883042c6d0ea18830ef238e2cf3
takagi <209098532@qq.com>
2019-09-20 10:13:39 +0800

    页面重用、修改功能的实现

--- boot-web/src/main/java/com/takagi/bootweb/controller/EmployeeController.java
+++ boot-web/src/main/java/com/takagi/bootweb/controller/EmployeeController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -38,6 +40,23 @@ public class EmployeeController {
 
     @PostMapping("/emp")
     public String addEmp(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id,Model model) {
+        //查询部门信息
+        Collection<Department> depts = departmentDao.getDepartments();
+        model.addAttribute("depts",depts);
+        //获取员工信息
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp", employee);
+        return "emp/add";
+    }
+
+    @PutMapping("/emp")
+    public String saveEmp(Employee employee) {
         employeeDao.save(employee);
         return "redirect:/emps";
     }
